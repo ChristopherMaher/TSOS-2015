40123b45f36003ade2327c1c755045896c1fc8fe
Chris Maher <christopher.maher13@gmail.com>
2015-11-04 16:51:57 -0500

    fixes #18

--- source/.gulp-tsc-tmp-115101-9798-498r8m.js
+++
@@ -1,2 +0,0 @@
-// This is a temporary file by gulp-tsc for keeping directory tree.
-//# sourceMappingURL=.gulp-tsc-tmp-115101-9798-498r8m.js.map
--- source/os/scheduler.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Chris on 11/1/15.
- */
-//# sourceMappingURL=scheduler.js.map
